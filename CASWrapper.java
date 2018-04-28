c52f1ec1809dbe68cc68d6b5702aba8eb436e87f
Norman Gilmore <git@teamforecast.com>
2018-04-27 17:11:15 -0700

    Change CASWrapper from a static method to a subclass of CodingAnnotationStudy.

--- CASWrapper.java
+++ CASWrapper.java
@@ -1,17 +1,16 @@
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.coding.CodingAnnotationStudy;
+import org.dkpro.statistics.agreement.coding.CodingAnnotationStudy;
 
+public class CASWrapper extends CodingAnnotationStudy {
+    public CASWrapper(int raterCount) {
+      super(raterCount);
+    }
 
-
-public class CASWrapper {
-    public static CodingAnnotationStudy makeStudy(int[] input){
+    public void addAsArray(int[] input){
         Integer[] bigIInput = new Integer[input.length];
         for (int i = 0; i< input.length; i+=1){
             Integer member = new Integer(input[i]);
             bigIInput[i] = member;
         }
-        CodingAnnotationStudy study = new CodingAnnotationStudy(input.length);
-        study.addItemAsArray(bigIInput);
-        return study;
+        this.addItemAsArray(bigIInput);
     }
-
 }
