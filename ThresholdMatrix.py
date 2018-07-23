359e2014d764670e4eede56511cca6b64f6b177b
EricWimsatt <ericwimsatt@gmail.com>
2018-07-23 14:49:35 -0700

    rep integreation

--- ThresholdMatrix.py
+++ ThresholdMatrix.py
@@ -1,5 +1,6 @@
 from math import exp
-def evalThresholdMatrix(percentage, num_of_users):
+def evalThresholdMatrix(percentage, num_of_users, scale = 1):
+    percentage = percentage*scale
     kappa = 2+1 *(1 - exp((-num_of_users)/50))
     q = 1
     #omega = 1/1+exp(-kappa*(percentage -.5))
@@ -10,3 +11,10 @@ def evalThresholdMatrix(percentage, num_of_users):
         return 'H'
     else:
         return 'L'
+def checkThreshold():
+    for i in range(3,8):
+        for u in range(3,10):
+            p = i/10
+            print(str(p)+"%", u, 'users')
+            print(evalThresholdMatrix(p, u))
+            print(evalThresholdMatrix(p, u, 1.4))
--- TriagerScoring.py
+++ TriagerScoring.py
@@ -1,20 +1,102 @@
 import numpy as np
 from UnitizingScoring import *
 from ThresholdMatrix import *
+import pandas as pd
+from math import floor
 
-def scoreTriager(starts,ends, length, users, numUsers, inFlags, categories):
+path1 = 'SemanticsTriager1.3C2-2018-07-19T17.csv'
+path2 = 'FormTriager1.2C2-2018-07-19T17.csv'
 
-
+def scoreTriager(starts,ends, users, numUsers, inFlags, length, flagExclusions):
 
     #TODO: do this for each category
-    passers = findPassingIndices(starts, ends, length, numUsers, users)
+    passers = findPassingIndices(starts, ends, numUsers, users, length)
+    #catExclusions = exclusionList(users, flags, minU = 5)
+    #flagExclusions.append()
+    excl = findExcludedIndices(flagExclusions, users)
+    print(excl)
+    if len(excl > 1):
+        inFlags = exclude(excl, inFlags)
+        coded = exclude(excl, coded)
     codetoUser, userToCode= codeNameDict(users)
     coded = codeUsers(userToCode, users)
-    newStarts, newEnds = toStartsEnds(passers)
-    flags = determineFlags(starts, ends, newStarts, newEnds, coded, inFlags)
-    print(newStarts, newEnds, flags)
-    return 0
+    print(coded, numUsers)
 
+    newStarts, newEnds = toStartsEnds(passers)
+    #Exclude flags from users who didn't use case flags
+
+
+    flags = determineFlags(starts, ends, newStarts, newEnds, coded, inFlags)
+    print('Starts:',newStarts)
+    print('Ends:',newEnds)
+    print('Flags:', flags)
+    print('---------------------------')
+    return newStarts, newEnds,flags
+
+def importData(path):
+    data = pd.read_csv(path)
+    article_nums = np.unique(data['task_pybossa_id'])
+
+    for a in article_nums:
+        print('---------------------------')
+        art_data = data.loc[data['task_pybossa_id'] == a]
+        annotator_count = len(np.unique(art_data['contributor_id']))
+        users = art_data['contributor_id'].tolist()
+        flags = art_data['case_number'].tolist()
+        cats = art_data['topic_name'].tolist()
+        flagExclusions = exclusionList(users, flags, cats)
+        print(flagExclusions)
+        if annotator_count >= 2:
+            cats = np.unique(art_data['topic_name'])
+            for c in cats:
+
+                cat_data = art_data.loc[art_data['topic_name'] == c]
+                starts = [int(s) for s in cat_data['start_pos'].tolist()]
+                ends = [int(e) for e in cat_data['end_pos'].tolist()]
+                users = cat_data['contributor_id'].tolist()
+                flags = cat_data['case_number'].tolist()
+                length = floor(cat_data['source_text_length'].tolist()[0])
+                numUsers = len(np.unique(users))
+                print('//Article:', a, 'Category:', c, 'numUsers:', numUsers)
+                scoreTriager(starts, ends, users, numUsers, flags, length, flagExclusions)
+
+    print("DONE")
+
+def findExcludedIndices(exclusions, users):
+    indices = np.array(())
+    for u in np.unique(users):
+        if u in exclusions:
+            uIndices = np.nonzero(users == u)
+            np.append(indices,uIndices)
+    #print(indices)
+    return indices
+def exclude(indices, arr):
+    # print(flags, len(flags))
+    # print('indices',indices)
+    arr = np.array(arr)
+    return np.delete(arr, indices)
+def exclusionList(users, flags,cats = None, minU= 8):
+    excluded = []
+    for u in np.unique(users):
+        myUserIndices = np.nonzero(users == u)[0]
+        # print(myUserIndices)
+        # print(np.array(users)[myUserIndices])
+        oneCount = 0
+        pot = 0
+        score = 0
+        #print(myUserIndices)
+        for i in myUserIndices:
+            #print(i)
+            if cats!=None and cats[i]!= 'Language':
+                if flags[i] == 1:
+                    oneCount+=1
+                pot = pot +1
+            if pot>0:
+                score = oneCount/pot
+        if score>.8 and pot > minU:
+            excluded.append(u)
+    #print('excl', excluded,'users', np.unique(users))
+    return excluded
 
 def codeNameDict(users):
     uqUsers = np.sort(np.unique(users))
@@ -31,13 +113,14 @@ def codeUsers(userDict, users):
         coded.append(userDict[u])
     return coded
 
-def findPassingIndices(starts, ends, length, numUsers, users):
+def findPassingIndices(starts, ends, numUsers, users, length):
+    #print(starts)
     answerMatrix = toArray(starts, ends, length, numUsers, users, None)
     percentageArray = scorePercentageUnitizing(answerMatrix, length, numUsers)
     passersArray = np.zeros(len(percentageArray))
     # TODO: instead of passing to threshold matrix each time, just find out minScoretoPass
     for i in range(len(percentageArray)):
-        if evalThresholdMatrix(percentageArray[i], numUsers) == 'H':
+        if evalThresholdMatrix(percentageArray[i], numUsers, 1.4) == 'H':
             passersArray[i] = 1
     return passersArray
 
@@ -52,6 +135,8 @@ def toStartsEnds(passers):
             elif prev == 1:
                 ends.append(i-1)
             prev = 1-prev
+    if len(ends)<len(starts):
+        ends.append[passers[-1]]
     return starts, ends
 
 def toFlagMatrix(starts, ends, nStarts, nEnds, codedUsers, flags):
@@ -64,14 +149,17 @@ def toFlagMatrix(starts, ends, nStarts, nEnds, codedUsers, flags):
             startsUsersDict[start] = startsUsersDict[start].append(index)
 
     startsUsersDict = {}
-    flagMatrix = np.zeros((len(nStarts), len(np.unique(codedUsers))))
-    #i corresponds tot he code of a user
-    for i in np.arange(len(starts)):
-        for j in np.arange(len(nStarts)):
-            if (starts[i] < nStarts[j] and ends[i] > nStarts[j]) or \
-                    (starts[i]< nEnds[j] and starts[i] > nStarts[j]):
-                flagMatrix[j][codedUsers[i]] = flags[i]
-    return flagMatrix.T
+    if len(nStarts)>0 and len(np.unique(codedUsers)>0):
+        flagMatrix = np.zeros((len(nStarts), len(np.unique(codedUsers))))
+        #i corresponds tot he code of a user
+
+        for i in np.arange(len(starts)):
+            for j in np.arange(len(nStarts)):
+                if (starts[i] < nStarts[j] and ends[i] > nStarts[j]) or \
+                        (starts[i]< nEnds[j] and starts[i] > nStarts[j]):
+                    flagMatrix[j][codedUsers[i]] = flags[i]
+        return flagMatrix.T
+    return []
 
 def assignFlags(matrix):
     numUsers = len(matrix)
@@ -89,11 +177,11 @@ def assignFlags(matrix):
                     score = 0
                     potential = 0
                     for u in range(numUsers):
-                        if matrix[u][i] != 0 and matrix[u][j] != 0:
+                        if matrix[u][i] != 0 and matrix[u][j] != 0 :
                             potential += 1
-                        if matrix[u][i] == matrix[u][j]:
-                            score += 1
-                    if score/potential >= .5:
+                            if matrix[u][i] == matrix[u][j]:
+                              score += 1
+                    if potential > 0 and score/potential >= .5:
                         flags[j] = flags[i]
                         sortedNStarts.append(j)
     return flags
@@ -101,17 +189,28 @@ def assignFlags(matrix):
 
 def determineFlags(starts, ends, nStarts, nEnds, codedUsers, flags):
     matrix = toFlagMatrix(starts, ends, nStarts,nEnds, codedUsers, flags)
-    outFlags = assignFlags(matrix)
-    return outFlags
+    #print(matrix)
+    if len(matrix)>0 and len(matrix[0]>0):
+        outFlags = assignFlags(matrix)
+        return outFlags
+    return []
 
-s = [5, 45, 3, 80, 6, 65]
 
-e1 = [30,100, 30,100, 30,100]
-l = 120
-u = [4,4,7,7,3,3]
-f = [1,3,4,6,7,2]
-f2 = [1,1,4,4,7,7]
-c = 3
+print("#####SEMANTICS TRIAGER AGREED UPON DATA!!!#####")
+importData(path1)
 
-scoreTriager(s,e1,l,u,3,f,c)
-scoreTriager(s,e1,l,u,3,f2,c)
+print()
+print()
+#print("#####FORM TRIAGER AGREED UPON DATA!!!#####")
+#importData(path2)
+# s = [5, 45, 3, 80, 6, 65]
+#
+# e1 = [30,100, 30,100, 30,100]
+# l = 120
+# u = [4,4,7,7,3,3]
+# f = [1,3,4,6,7,2]
+# f2 = [1,1,4,4,7,7]
+# c = 3
+
+# scoreTriager(s,e1,l,u,3,f,c)
+# scoreTriager(s,e1,l,u,3,f2,c)
