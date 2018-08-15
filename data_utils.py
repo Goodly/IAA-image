2766089ff02d347869d160c828f947a7b26ac330
Eric Wimsatt <ericwimsatt@gmail.com>
2018-08-14 21:33:32 -0700

    extra outputs for readiability

--- data_utils.py
+++ data_utils.py
@@ -35,11 +35,11 @@ def data_storer(path):
     """Function that turns csv data. Input the path name for the csv file.
     This will return a super dictionary that is used with other abstraction functions."""
 
-    data = pd.read_csv(path)
-    article_nums = np.unique(data['taskrun_article_number'])
+    data = pd.read_csv(path, encoding = 'utf-8')
+    article_nums = np.unique(data['article_sha256'])
     dict_of_article_df = dict()
     for i in article_nums:
-        article =data[data['taskrun_article_number'] == i]
+        article =data[data['article_sha256'] == i]
         question_nums = np.unique(article['question_number'])
         new_dict = dict()
         for x in question_nums:
@@ -47,11 +47,18 @@ def data_storer(path):
             array.append(article.loc[article['question_number']== x, 'question_text'][0:1])
 
             answers = [article.loc[article['question_number']== x, 'answer_number']]
-            answers.append([article.loc[article['question_number']== x, 'contributor_id']])
+            answers.append([article.loc[article['question_number']== x, 'contributor_uuid']])
             answers.append([article.loc[article['question_number']== x, 'start_pos']])
             answers.append([article.loc[article['question_number']== x, 'end_pos']])
             answers.append([article.loc[article['question_number']== x, 'source_text_length']])
-            answers.append([article.loc[article['question_number'] == x, 'answer_type']])
+            #Change topic_name to answer_type when we get that feature request
+            answers.append([article.loc[article['question_number'] == x, 'topic_name']])
+            answers.append([article.loc[article['question_number'] == x, 'article_number']])
+            answers.append([article.loc[article['question_number'] == x, 'namespace']])
+            answers.append([article.loc[article['question_number'] == x, 'target_text']])
+            answers.append([article.loc[article['question_number'] == x, 'question_text']])
+            answers.append([article.loc[article['question_number'] == x, 'answer_content']])
+
             array.append(answers)
 
             new_dict[x] = array
@@ -59,6 +66,28 @@ def data_storer(path):
         dict_of_article_df[i] = new_dict
     return dict_of_article_df
 
+# Deprecated version, may be useful if we need to re-run the huge set of test cases
+# def data_storer(path):
+#     """Function that turns csv data. Input the path name for the csv file.
+#     This will return a super dictionary that is used with other abstraction functions."""
+#
+#     data = pd.read_csv(path, encoding = 'utf-8')
+#     article_nums = np.unique(data['taskrun_article_number'])
+#     dict_of_article_df = dict()
+#     for i in article_nums:d
+
+#             answers = [article.loc[article['question_number']== x, 'answer_number']]
+#             answers.append([article.loc[article['question_number']== x, 'contributor_id']])
+#             answers.append([article.loc[article['question_number']== x, 'start_pos']])
+#             answers.append([article.loc[article['question_number']== x, 'end_pos']])
+#             answers.append([article.loc[article['question_number']== x, 'source_text_length']])
+#             answers.append([article.loc[article['question_number'] == x, 'answer_type']])
+#             array.append(answers)
+#
+#             new_dict[x] = array
+#             #this is where krippendorf goes
+#         dict_of_article_df[i] = new_dict
+#     return dict_of_article_df
 
 #Abstraction functions for the data structure
 def get_question_answers(data, article_num, question_num):
@@ -88,7 +117,71 @@ def get_num_users(data, article_num, question_num):
 
 def get_question_type(data, article_num, question_num):
     return data[article_num][question_num][1][5][0].iloc[0]
-#Retuns dictionary of cleaned up data
+
+def get_article_num(data, article):
+
+    q  = min(data[article].keys())
+    return data[article][q][1][6][0].iloc[0]
+
+
+def get_namespace(data, article, question_num):
+    return data[article][question_num][1][7][0].iloc[0]
+
+def get_answer_texts(data, article_num, question_num):
+    return data[article_num][question_num][1][8][0].tolist()
+
+def get_question_text(data, article_num, question_num):
+    return data[article_num][question_num][1][9][0].iloc[0]
+
+def get_answer_content(data, article_num, question_num, answer_num):
+    if answer_num == 'U' or answer_num == 'L' or answer_num == 'M' or answer_num == 'N/A':
+        return answer_num
+
+    answers = get_question_answers(data, article_num, question_num).tolist()
+
+    index = finder(answers, answer_num)
+
+    if index<0:
+        return 'N/A'
+    answer_contents = data[article_num][question_num][1][10][0]
+    desired_content = answer_contents.tolist()[index]
+    return desired_content
+
+def finder(ser, a):
+    if len(ser)<1:
+        return -1
+    for i in range(len(ser)):
+            if ser[i]==a:
+                    return i
+    return -1
+
+def get_type_hard(type, ques):
+    #TODO:verify all of this against the schema
+    typing_dict = {
+        'Source relevance':
+            {
+                1: ['checklist', 7],
+                2: ['checklist', 8],
+                3: ['ordinal', 5]
+            },
+        'Science and Causality Questions for SSS Students V2':
+            {
+                5:['nominal', 1],
+                6: ['checklist', 8],
+                9:['ordinal', 4],
+                10:['nominal', 1]
+            },
+        'Language Specialist V2':
+            {
+                1:['checklist',9],
+                6:['checklist', 8]
+            }
+
+    }
+
+    out = typing_dict[type][ques]
+    return out[0], out[1]
+
 def cleanForUnitization(data, article_num, question_num):
     """Retuns dictionary of cleaned up data. Keys are Offsets, Lengths, Categories, Raters, and Ends
     and each value is an array of the data. DEPRECATED."""
