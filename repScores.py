b7fbd1bcee0f076ed5e2e14195db5b8ece3a359e
Alex Popescu <alexcpopescu7@gmail.com>
2018-08-17 18:54:16 -0700

    pulling from master

--- repScores.py
+++ repScores.py
@@ -15,60 +15,39 @@ def create_user_dataframe(data, csvPath=None):
         for question_num in data[article_num].keys():
             users_q = get_question_userid(data, article_num, question_num)
             for ids in users_q:
-                ids = int(ids)
                 if ids not in data1.loc[:, 'Users'].tolist():
                     data1 = data1.append({"Users": ids, "Score": 5, "Questions": 1, "Influence": 1}, ignore_index=True)
     return data1
 
 
-def gaussian_mean(answers):
-    result = []
-    std = np.std(answers)
-    mean = np.mean(answers)
-    total = 0
-    for i in answers:
-        gauss = 1 / (std * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((i - mean) / std) ** 2)
-        result.append(i * (gauss * 10) ** 2)
-        total += (gauss * 10) ** 2
-    print(result)
-    print(answers)
-    print(np.mean(answers))
-    print(sum(result) / total)
-    return sum(result) / total
-
-def do_rep_calculation_ordinal(userID, answers, answer_aggregated, num_of_choices, highlight_answer, starts, ends,
-                               article_length, data):
+def do_rep_calculation_nominal(userID, answers, answer_choice, highlight_answer, starts, ends, article_length, data,
+                               checkListScale=1):
     """Using the same dataframe of userIDs, rep scores, and number of questions, changes the vals of the dataframe
-    such that the they receive the distance from the average answer chosen as a ratio of 0 to 1,
-    and that is added to their rep score."""
-    print(answer_aggregated)
-    if type(answer_aggregated)  == str or type(highlight_answer) == str:
+    such that if the user in the list of USERID gets their answer right, they add 1 to their score, and 0 if they are
+    wrong."""
+    if type(answer_choice)  == str or type(highlight_answer)  == str:
         return 0
     checked, int_users = checkDuplicates(answers, userID, starts, ends, article_length)
-    answers_passed = list()
-    highlight_answer_array = np.zeros(article_length)
-    score_dict = {}
     print(checked)
-    for i in checked:
-        answers_passed.append(i[1])
-    answer_choice = gaussian_mean(answers)
-
-    for h in highlight_answer:
-        print(h)
-        highlight_answer_array[h] = 1
-
+    print(int_users)
+    highlight_answer_array = np.zeros(article_length)
+    winners = []
     for t in checked:
         user = t[1]
         answer = t[0]
-        points = (1 - abs(answer_choice - answer) / num_of_choices) ** 3
-        do_math(data, user, points)
-        score_dict[user] = points
+        print(answer, answer_choice, user)
+        if (answer == answer_choice):
+            do_math(data, user, checkListScale)
+            winners.append(user)
+        else:
+            do_math(data, user, 0)
+    for h in highlight_answer:
+        highlight_answer_array[h] = 1
     for x in int_users:
-        points = score_dict[x]
-        highlight = np.array(int_users[x])
-
-        score = points * (1 - np.sum(np.absolute(highlight_answer_array - highlight)) / article_length)
-        do_math(data, x, score)
+        if x in winners:
+            highlight = np.array(int_users[x])
+            score = 1 - np.sum(np.absolute(highlight_answer_array - highlight)) / article_length
+            do_math(data, x, score)
 
 
 def gaussian_mean(answers):
