f198048fb89b49c7778c7760469e810b76260f0c
Eric Wimsatt <ericwimsatt@gmail.com>
2019-10-11 14:50:20 -0700

    public read access

--- send_to_s3.py
+++ send_to_s3.py
@@ -5,6 +5,6 @@ def send_s3(scoring_dir):
     for root, dir, files in os.walk(scoring_dir):
         for file in files:
             if file.endswith('.csv') and 'VisualizationData_' in file:
-                cmd_str = "aws s3 mv "+ scoring_dir+'/'+file+" s3://publiceditor.io/Articles/"+file
+                cmd_str = "aws s3 mv "+ scoring_dir+'/'+file+" s3://publiceditor.io/Articles/"+file+" --acl public-read"
                 os.system(cmd_str)
                 print('calling:',cmd_str)
