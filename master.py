e4a9aaf4f03c2abbe17c9babbd78a0f0c6199de9
Eric Wimsatt <ericwimsatt@gmail.com>
2019-10-11 14:48:20 -0700

    sends to aws

--- master.py
+++ master.py
@@ -9,9 +9,13 @@ from Separator import *
 from IAA_report import make_iaa_human_readable
 from dataV2 import make_directory
 from time import time
+from send_to_s3 import send_s3
+
+
 def calculate_scores_master(directory, tua_file = None, iaa_dir = None, scoring_dir = None, repCSV = None,
                             just_s_iaa = False, just_dep_iaa = False, use_rep = False, reporting  = True,
-                            single_task = False, highlights_file = None, schema_file = None, answers_file = None):
+                            single_task = False, highlights_file = None, schema_file = None, answers_file = None,
+                            push_aws = True):
     """
 
     :param directory: the directory that holds all files from the tagworks datahunt export
@@ -35,6 +39,7 @@ def calculate_scores_master(directory, tua_file = None, iaa_dir = None, scoring_
         that is output from tagworks
     **if in the future the data import is adjusted to depend on other file outputs from tagworks, new parameters would
         have to be added to accomodate the change in importing procedures
+    :param push_aws: True if we want outputs sent to the s3 AWS folder, false to just store locally
     :return: No explicit return.  Running will create two directories named by the inputs. the iaa_dir will house
         a csv output from the IAA algorithm.  The scoring_dir will house the csvs output from the dependency evaluation
         algorithm; the weighting algorithm; the point sorting algorithm; and the final cleaning algorithm that prepares
@@ -44,6 +49,7 @@ def calculate_scores_master(directory, tua_file = None, iaa_dir = None, scoring_
     #iaa_dir is now handled inside IAA.py
     #if iaa_dir is None:
     #    iaa_dir = 's_iaa_'+directory
+    print("PUSH AWS VAL", push_aws)
     rep_direc = directory + "_report"
     make_directory(rep_direc)
     start = time()
@@ -73,6 +79,10 @@ def calculate_scores_master(directory, tua_file = None, iaa_dir = None, scoring_
     print("----------------SPLITTING-----------------------------------")
     splitcsv(scoring_dir)
     #print("DONE, time elapsed", time()-start)
+    if push_aws:
+        print("Pushing to aws")
+        send_s3(scoring_dir)
+
 
 def load_args():
     parser = argparse.ArgumentParser()
@@ -116,12 +126,15 @@ def load_args():
     parser.add_argument(
         '-af', '--answers_file',
         help="only used if single_task is true; necessary if single_task is true; the path to the answers file that is output from tagworks")
+    parser.add_argument(
+        '-p', '--push_aws',
+        help= "true if you want to push the visualization data to the aws s3; false by default")
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = load_args()
-    input_dir = 'sep_urap_evi'
+    #input_dir = 'sep_urap_evi'
     tua_file = './config/allTUAS.csv'
     output_dir = None
     scoring_dir  = None
