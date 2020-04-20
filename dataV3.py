7507c0f890890e01c0292bbf212fdcf8b9e77264
Danji Liu <daniel-eg2501@berkeley.edu>
2020-04-20 15:12:25 -0700

    fix hlusers, targettexts

--- dataV3.py
+++ dataV3.py
@@ -90,6 +90,10 @@ def dataStorer(data_hunt_path, schema_path):
     highlight = data_hunt.loc[:, ["quiz_task_uuid", "question_label", "contributor_uuid",
                                   "target_text", "highlight_count", "answer_uuid", "answer_content", "answer_label", "answer_text"]
                 ].set_index("quiz_task_uuid")
+    highlighted_data = data_hunt.loc[:, ["quiz_task_uuid", "question_label", "contributor_uuid",
+                                  "target_text", "highlight_count", "answer_uuid", "answer_content", "answer_label", "answer_text"]
+                ]
+    highlighted_data = highlighted_data[highlighted_data["highlight_count"] > 0]
 
     schemaData = pd.read_csv(schema_path, encoding = 'utf-8')
 
@@ -146,28 +150,41 @@ def dataStorer(data_hunt_path, schema_path):
         # gets highlighted information
         usersGrouped = highlight.loc[uuid].groupby("question_label")
         contributors = usersGrouped["contributor_uuid"].apply(list)
-        highlightedTasks = highlight[highlight["highlight_count"] > 0]
 
-        hasHighlightedTasks = False
-        if uuid in highlightedTasks:
-            hl_users_data = highlightedTasks.loc[uuid].groupby("question_label")["contributor_uuid"].apply(list)
-            hl_ans_labels_data = highlightedTasks.loc[uuid].groupby("question_label")["answer_label"].apply(list)
-            hasHighlightedTasks = True
-        target_text = usersGrouped["target_text"].apply(list) # Don't know for sure
+        #
+        # hasHighlightedTasks = False
+        # if uuid in highlighted_data['quiz_task_uuid']:
+        #     rel_hl_data = highlighted_data[highlighted_data['quiz_task_uuid'] == uuid]
+        #     hl_users_data = rel_hl_data.groupby("question_label")["contributor_uuid"].apply(list)
+        #     hl_ans_labels_data = rel_hl_data.groupby("question_label")["answer_label"].apply(list)
+        #     hasHighlightedTasks = True
+
+            # target_text = usersGrouped["target_text"].apply(list) # Don't know for sure
         answer_content = highlight.loc[uuid].groupby("question_label")["answer_content"].apply(list)
         newUsers = highlight.loc[uuid].groupby("question_label")["contributor_uuid"].apply(list)
-
+        rel_hl_data = highlighted_data[highlighted_data['quiz_task_uuid'] == uuid]
         # for each task, creates a new questionData that parses through related question and answer
         ques = uberDict[uuid]['quesData'] = {}
         for question_label in list(starts.index):
             hightlightUsers = []
             hl_ans_nums = []
-            hl_ans_labels = []
-            if hasHighlightedTasks:
-                if question_label in hl_users_data.index:
-                    hightlightUsers = hl_users_data[question_label]
-                    hl_ans_labels = hl_ans_labels_data[question_label]
-                    hl_ans_nums = [getAnsNumberFromLabel(l) for l in hl_ans_labels]
+            target_text = []
+
+            # print("Indices of highlight data under task", uuid, "are", rel_hl_data["question_label"])
+            # print("The question label is: ", question_label)
+            if rel_hl_data["question_label"].str.contains(question_label).any():
+                # print("The indices are:", hl_users_data.index)
+                question_hl_data = rel_hl_data[rel_hl_data["question_label"] == question_label]
+                hightlightUsers = question_hl_data["contributor_uuid"].tolist()
+                hl_ans_labels = question_hl_data["answer_label"].tolist()
+                hl_ans_nums = [getAnsNumberFromLabel(l) for l in hl_ans_labels]
+                target_text = question_hl_data["target_text"].tolist()
+
+                # print("Highlight Users are: ", hightlightUsers)
+                # print("Highlight Answers are: ", hl_ans_labels)
+                # print("Highlight target texts are: ", target_text)
+
+
             question_num = getQuestionNumberFromLabel(question_label)
 
             ques[question_num] = {
@@ -178,7 +195,7 @@ def dataStorer(data_hunt_path, schema_path):
                     "numUsers" : len(newUsers[question_label]),
                     "answer_content": find_answer_contents(task_schema, question_label),
                     "question_text": quest_label_text.loc[question_label, "question_text"],
-                    "target_text": target_text[question_label], #TODO: currently returns a list of target texts, check!
+                    "target_text": target_text, #TODO: currently returns a list of target texts, check!
                     "hlUsers": hightlightUsers,
                     'parents': dependencyStatus(dependencies, question_num),
                     "numChoices" : 10,
